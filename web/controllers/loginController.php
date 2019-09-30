cd8d340d3eb18cfdba08d5953da95d75016b434c
vereczkibalint <vereczkibalint@gmail.com>
2019-09-30 14:21:28 +0200

    LoginController added

--- web/controllers/loginController.php
+++ web/controllers/loginController.php
@@ -5,5 +5,23 @@ if(isset($_POST["login_submit"])){
     $username = trim($_POST["username"]);
     $password = trim($_POST["password"]);
 
-    
+    $db = new Database();
+    $connection = $db->getConnection();
+
+    $sql = "SELECT * FROM felhasznalok WHERE felhasznalonev = ?";
+    $stmt = $connection->prepare($sql);
+    $stmt->bindParam(1, $username);
+    $stmt->execute();
+
+    if($stmt->rowCount() > 0){
+        $userdata = $stmt->fetch(PDO::FETCH_ASSOC);
+        if(password_verify($password, $userdata["jelszo"])){
+            // sikeres belépés
+            // TODO: set Session
+            // TODO: redirect home
+        }else{
+            // sikertelen belépés
+            // TODO: redirect login
+        }
+    }
 }
