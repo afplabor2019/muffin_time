135bfa3c2f0139e48dd53b063f48d7a196f6fb00
vereczkibalint <vereczkibalint@gmail.com>
2019-10-05 20:19:01 +0200

    Added create new user function

--- web/views/dashboard.php
+++ web/views/dashboard.php
@@ -23,6 +23,7 @@ if(Session::isset('userid')){
                 if(User::isAdmin(Session::get('userid'))){
                     $users = User::getAllUser();
             ?>
+            <h1>Listing all users</h1>
                 <div>
                     <table class="table centered">
                         <thead>
@@ -49,6 +50,31 @@ if(Session::isset('userid')){
                         </tbody>
                     </table>
                 </div>
+                
+                <?php
+                    if(isset($_POST["newuser_submit"])){
+                        $username = trim($_POST["username"]);
+                        $password1 = trim($_POST["password1"]);
+                        $password2 = trim($_POST["password2"]);
+                        $email = trim($_POST["email"]);
+                        $role = ($_POST["role"] == "") ? "felhasznalo" : $_POST["role"];
+                    
+                        User::create($username,$password1,$password2,$email,$role);
+                    }
+                ?>
+                <h1>Create New User</h1>
+                <form method="post">
+                    <input type="text" id="username" name="username" placeholder="username" required>
+                    <input type="password" id="password1" name="password1" placeholder="password" required>
+                    <input type="password" id="password2" name="password2" placeholder="confirm password" required>
+                    <input type="email" id="email" name="email" placeholder="email address" required>
+                    <select name="role" id="role" required>
+                        <option value="" disabled selected>-- choose role--</option>
+                        <option value="felhasznalo">user</option>
+                        <option value="admin">admin</option>
+                    </select>
+                    <input type="submit" value="Submit" name="newuser_submit">
+                </form>
             <?php
                 }
             ?>
