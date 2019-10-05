d37b85f481d07fa8224e3707673af1d2c003d531
vereczkibalint <vereczkibalint@gmail.com>
2019-10-05 10:24:14 +0200

    Include all necessary classes

--- web/config/init.php
+++ web/config/init.php
@@ -3,5 +3,4 @@ session_start();
 include_once 'classes/Database.php';
 include_once 'classes/Redirect.php';
 include_once 'classes/Session.php';
-
-define('DOMAIN', 'http://localhost/afplabor_final/');
+include_once 'classes/Message.php';
