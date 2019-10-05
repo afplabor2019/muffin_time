7f7580311da9effdf14b6919e9c01d17255e76f4
vereczkibalint <vereczkibalint@gmail.com>
2019-10-05 10:58:06 +0200

    Added admin panel if the user has permission

--- web/views/dashboard.php
+++ web/views/dashboard.php
@@ -2,11 +2,22 @@
 include_once 'config/init.php';
 include_once '_header.php';
 ?>
+<?php
+if(Session::isset('userid')){
+?>
 
 <h1>Dashboard</h1>
-Üdvözöllek, <?=$_SESSION['userid']?>!
+Üdvözöllek, <?=Session::get('userid')?>!
+<?php
+if(User::isAdmin(Session::get('userid'))){
+    echo "<a href='?p=admin'>Admin panel</a> ";
+}
+?>
 <a href="?p=logout">Kilépés</a>
 
+<?php
+}
+?>
 <?php
 include_once '_footer.php';
 ?>
