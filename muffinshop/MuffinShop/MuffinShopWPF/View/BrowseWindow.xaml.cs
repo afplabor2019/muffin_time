d9dbe97515b879f37e4ade0da400311fa753d558
vereczkibalint <vereczkibalint@gmail.com>
2019-11-05 11:20:41 +0100

    Example database management in EF

---
+++ muffinshop/MuffinShop/MuffinShopWPF/View/BrowseWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MuffinShopWPF.Model;
+using System.Data.Entity;
+
+namespace MuffinShopWPF.View
+{
+    /// <summary>
+    /// Interaction logic for BrowseWindow.xaml
+    /// </summary>
+    public partial class BrowseWindow : Window
+    {
+        MuffinContext context = new MuffinContext();
+        CollectionViewSource muffinViewSource;
+
+        public BrowseWindow()
+        {
+            InitializeComponent();
+            muffinViewSource = ((CollectionViewSource)(FindResource("muffinsViewSource")));
+            DataContext = this;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            context.muffins.Load();
+            muffinViewSource.Source = context.muffins.Local;
+        }
+    }
+}
