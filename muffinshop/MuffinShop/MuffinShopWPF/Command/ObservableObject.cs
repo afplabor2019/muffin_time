5092192d56f23a1096e8c1425235a5f6ab90c5e7
vereczkibalint <vereczkibalint@gmail.com>
2019-11-07 20:21:36 +0100

    Custom class for MVVM purposes

---
+++ muffinshop/MuffinShop/MuffinShopWPF/Command/ObservableObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuffinShopWPF.Command
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged(String info)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
