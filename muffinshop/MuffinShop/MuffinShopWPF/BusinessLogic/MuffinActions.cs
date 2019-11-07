b62e4a0119339cfdb35c9f92e817fc4e65d0938d
vereczkibalint <vereczkibalint@gmail.com>
2019-11-07 20:19:39 +0100

    Created class with add, modify, delete functions

---
+++ muffinshop/MuffinShop/MuffinShopWPF/BusinessLogic/MuffinActions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MuffinShopWPF.Model;
+
+namespace MuffinShopWPF.BusinessLogic
+{
+    public static class MuffinActions
+    {
+        public static void AddMuffin(string muffin_name, decimal muffin_price)
+        {
+            using(var context = new MuffinContext())
+            {
+                Muffin newMuffin = new Muffin();
+                newMuffin.muffin_name = muffin_name;
+                newMuffin.muffin_price = muffin_price;
+                context.Muffin.Add(newMuffin);
+                context.SaveChanges();
+            }
+        }
+
+        public static void ModifyMuffin(int muffin_id, string muffin_name, decimal muffin_price)
+        {
+            using(var context = new MuffinContext())
+            {
+                Muffin modifyMuffin = context.Muffin.Find(muffin_id);
+                modifyMuffin.muffin_name = muffin_name;
+                modifyMuffin.muffin_price = muffin_price;
+                context.Entry(modifyMuffin).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public static void DeleteMuffin(int id)
+        {
+            using(var context = new MuffinContext())
+            {
+                Muffin deleteMuffin = context.Muffin.Find(id);
+                context.Muffin.Remove(deleteMuffin);
+                context.SaveChanges();
+            }
+        }
+    }
+}
