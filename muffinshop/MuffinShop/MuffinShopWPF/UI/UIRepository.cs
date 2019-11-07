a7a0ff39641785efdad9daa3fc69a0ede39fdb42
vereczkibalint <vereczkibalint@gmail.com>
2019-11-07 20:24:51 +0100

    Created UIRepository to save the logged in user's ID

---
+++ muffinshop/MuffinShop/MuffinShopWPF/UI/UIRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuffinShopWPF.UI
+{
+    class UIRepository
+    {
+        private static volatile UIRepository instance;
+        private static object syncRoot = new object();
+
+        private UIRepository() { }
+
+        public static UIRepository Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new UIRepository();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        private int _currentClientID;
+        public int CurrentClientID
+        {
+            get { return _currentClientID; }
+            set { _currentClientID = value; }
+        }
+    }
+}
