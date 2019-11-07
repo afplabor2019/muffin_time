57ef9398bc8dc04c759408ddd361e8bb934f47e4
vereczkibalint <vereczkibalint@gmail.com>
2019-11-07 20:24:17 +0100

    Created model file for Muffin table in the database

---
+++ muffinshop/MuffinShop/MuffinShopWPF/Model/MuffinModelCS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace MuffinShopWPF.Model
+{
+    class MuffinModelCS
+    {
+        public int muffin_id { get; set; }
+        public string muffin_name { get; set; }
+        public Nullable<decimal> muffin_price { get; set; }
+    }
+}
