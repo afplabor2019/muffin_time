e6a31a678c86666c9724b24bcf547af53b375c9e
vereczkibalint <vereczkibalint@gmail.com>
2019-11-07 20:21:04 +0100

    Created eventaggregator to communicate with subscribed components

---
+++ muffinshop/MuffinShop/MuffinShopWPF/BusinessLogic/EventAggregator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuffinShopWPF.BusinessLogic
+{
+    public static class EventAggregator
+    {
+        public static void Broadcast(string message)
+        {
+            if(OnMessageTransmitted != null)
+            {
+                OnMessageTransmitted(message);
+            }
+        }
+
+        public static Action<string> OnMessageTransmitted;
+    }
+}
