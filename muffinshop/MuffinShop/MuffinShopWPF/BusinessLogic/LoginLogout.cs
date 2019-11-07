1006c85dc3e69803855c6fb86eb4b9196a208eb0
vereczkibalint <vereczkibalint@gmail.com>
2019-11-07 20:20:01 +0100

    Created LoginLogout function

---
+++ muffinshop/MuffinShop/MuffinShopWPF/BusinessLogic/LoginLogout.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MuffinShopWPF.Model;
+using MuffinShopWPF.BusinessLogic.Exceptions;
+using MuffinShopWPF.UI;
+
+namespace MuffinShopWPF.BusinessLogic
+{
+    class LoginLogout
+    {
+        public static bool UserLogin(string username, string password)
+        {
+            using (var context = new MuffinContext())
+            {
+                var issetUser = context.User.Any(user => user.username.ToLower() == username);
+                if (!issetUser)
+                    throw new UserNotFoundException();
+                else
+                {
+                    var user = context.User.Where(u => u.username.ToLower() == username.ToLower()).First();
+                    if (user.password != password)
+                        throw new IncorrectPasswordException();
+                    else
+                    {
+                        UIRepository.Instance.CurrentClientID = user.user_id;
+                        return true;
+                    }
+                }
+            }
+        }
+
+        public static void UserLogout()
+        {
+            UIRepository.Instance.CurrentClientID = 0;
+        }
+    }
+}
