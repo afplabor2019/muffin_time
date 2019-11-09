67ef586b2448dd9a7a2e076360b6f5aac6692ba3
vereczkibalint <vereczkibalint@gmail.com>
2019-11-09 20:47:44 +0100

    Created ViewModel for LoginRegWindow

---
+++ muffinshop/MuffinShop/MuffinShopWPF/ViewModel/LoginRegWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MuffinShopWPF.Command;
+using MuffinShopWPF.View;
+using MuffinShopWPF.Model;
+using MuffinShopWPF.UI;
+using MuffinShopWPF.BusinessLogic;
+
+namespace MuffinShopWPF.ViewModel
+{
+    class LoginRegWindowViewModel : ObservableObject
+    {
+        public LoginRegWindowViewModel()
+        {
+        }
+    }
+}
