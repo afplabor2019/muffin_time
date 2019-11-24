1013ea6a05e8a3f0eb3b04b55153fa209db561ab
vereczkibalint <vereczkibalint@gmail.com>
2019-11-24 19:48:12 +0100

    Created exception for existing email

---
+++ muffinshop/MuffinShop/MuffinShopWPF/BusinessLogic/Exceptions/EmailExistsException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuffinShopWPF.BusinessLogic.Exceptions
+{
+    class EmailExistsException : Exception
+    {
+        public EmailExistsException()
+        {
+        }
+
+        public EmailExistsException(string message)
+            : base(message)
+        {
+        }
+
+        public EmailExistsException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
