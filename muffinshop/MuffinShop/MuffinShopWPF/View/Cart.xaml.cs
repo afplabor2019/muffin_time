be8e59efc5ad81c8b352fbc05fd661ffe26bdd50
vereczkibalint <vereczkibalint@gmail.com>
2019-11-24 19:53:34 +0100

    Created Cart window

---
+++ muffinshop/MuffinShop/MuffinShopWPF/View/Cart.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MuffinShopWPF.View
+{
+    /// <summary>
+    /// Interaction logic for Cart.xaml
+    /// </summary>
+    public partial class Cart : Window
+    {
+        public Cart()
+        {
+            InitializeComponent();
+        }
+    }
+}
