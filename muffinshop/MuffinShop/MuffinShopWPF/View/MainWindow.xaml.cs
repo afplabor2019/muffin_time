3478366ce964e4f6ed3f9922f8d518085f53b499
vereczkibalint <vereczkibalint@gmail.com>
2019-11-24 19:56:01 +0100

    Implemented UI for MainWindow

--- muffinshop/MuffinShop/MuffinShopWPF/View/MainWindow.xaml.cs
+++ muffinshop/MuffinShop/MuffinShopWPF/View/MainWindow.xaml.cs
@@ -11,13 +11,12 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using MuffinShopWPF.Model;
-using System.Data.Entity;
+using MuffinShopWPF.UI;
 
 namespace MuffinShopWPF.View
 {
     /// <summary>
-    /// Interaction logic for BrowseWindow.xaml
+    /// Interaction logic for OneProduct.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -26,10 +25,48 @@ namespace MuffinShopWPF.View
             InitializeComponent();
         }
 
-        private void LogReg_Click(object sender, RoutedEventArgs e)
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            LoginRegWindow loginRegWindow = new LoginRegWindow();
-            loginRegWindow.Show();
+            int index = ListViewMenu.SelectedIndex;
+            MoveCursorMenu(index);
+            switch (index)
+            {
+                case 0:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new UserControlCakes());
+                    break;
+                case 1:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new UserControlChoose());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void MoveCursorMenu(int index)
+        {
+            TrainsitionigContentSlide.OnApplyTemplate();
+            GridCursor.Margin = new Thickness(0, (100+ (60* index)), 0, 0);
+        }
+
+        private void Btn_Basket_Click(object sender, RoutedEventArgs e)
+        {
+            Cart cart = new Cart();
+            cart.Show();
+        }
+
+        private void Btn_Account_Click(object sender, RoutedEventArgs e)
+        {
+            if(UIRepository.Instance.CurrentClientID == 0)
+            {
+                LoginRegWindow loginRegWindow = new LoginRegWindow();
+                loginRegWindow.Show();
+            }
+            else
+            {
+                MessageBox.Show(UIRepository.Instance.LoggedInUser.username);
+            }
         }
     }
 }
