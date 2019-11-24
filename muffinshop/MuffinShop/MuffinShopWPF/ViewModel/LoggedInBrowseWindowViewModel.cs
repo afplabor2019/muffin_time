97b1be71353edc18ee94e0d9ef9886d3ac7ed6df
vereczkibalint <vereczkibalint@gmail.com>
2019-11-24 19:57:55 +0100

    Created viewmodel for LoggedInBrowseWindow

---
+++ muffinshop/MuffinShop/MuffinShopWPF/ViewModel/LoggedInBrowseWindowViewModel.cs
@@ -0,0 +1,50 @@
+﻿using MuffinShopWPF.BusinessLogic;
+using MuffinShopWPF.Command;
+using MuffinShopWPF.Model;
+using MuffinShopWPF.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuffinShopWPF.ViewModel
+{
+    class LoggedInBrowseWindowViewModel : ObservableObject
+    {
+        public LoggedInBrowseWindowViewModel()
+        {
+            EventAggregator.OnMessageTransmitted += OnMessageReceived;
+        }
+
+        private void OnMessageReceived(string message)
+        {
+            if (message == "Muffin changed") NotifyPropertyChanged("AllMuffins");
+            if (message == "Muffin updated") NotifyPropertyChanged("AllMuffins");
+        }
+
+        public int UserId
+        {
+            get { return UIRepository.Instance.CurrentClientID; }
+        }
+
+        public User loggedInUser
+        {
+            get { return UIRepository.Instance.LoggedInUser; }
+        }
+
+        private ICollection<MuffinModelCS> _AllMuffins;
+        public ICollection<MuffinModelCS> AllMuffins
+        {
+            get
+            {
+                using (var context = new MuffinContext())
+                {
+                    _AllMuffins = MuffinMapper.EntityCollectionToModelCollection(context.Muffin.OrderByDescending(m => m.muffin_id).ToList());
+                }
+
+                return _AllMuffins;
+            }
+        }
+    }
+}
