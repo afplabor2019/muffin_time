9100bbd3ec48857bd12a36f35e2c65d831b28e48
vereczkibalint <vereczkibalint@gmail.com>
2019-11-24 19:51:33 +0100

    Created UserModel

---
+++ muffinshop/MuffinShop/MuffinShopWPF/Model/UserModelCS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuffinShopWPF.Model
+{
+    public class UserModelCS
+    {
+        public int user_id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public string zip_code { get; set; }
+        public string city { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+    }
+}
