f8d56d3216f50f192e6830c28992b3172297e90d
vereczkibalint <vereczkibalint@gmail.com>
2019-12-02 12:58:21 +0100

    Created Cart model

---
+++ muffinshop/MuffinShop/MuffinShopWPF/Model/Cart.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuffinShopWPF.Model
+{
+    class Cart
+    {
+        private ICollection<CartItem> cart;
+
+        public ICollection<CartItem> GetCart()
+        {
+            ICollection<CartItem> copyCart = new List<CartItem>();
+
+            if (cart.Count > 0)
+            {
+                foreach (CartItem item in cart)
+                {
+                    copyCart.Add(item);
+                }   
+            }
+
+            return copyCart;
+        }
+
+        public void AddItemToCart(CartItem item)
+        {
+            if (item != null)
+            {
+                cart.Add(item);
+            }
+        }
+
+        public void RemoveItemFromCart(CartItem item)
+        {
+            if (item != null && cart.Contains(item))
+            {
+                cart.Remove(item);
+            }
+        }
+
+        public void ModifyQty(CartItem item, byte qty)
+        {
+            if (item != null && cart.Contains(item))
+            {
+                var modifyItem = cart.Where(p => p.Product == item.Product).First();
+                modifyItem.Quantity += qty;
+            }
+        }
+
+        public Cart()
+        {
+
+        }
+    }
+}
