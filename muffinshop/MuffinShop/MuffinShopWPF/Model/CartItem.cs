85d1187af76c6d0bd601099852cd4f9e8191a49e
vereczkibalint <vereczkibalint@gmail.com>
2019-12-02 12:58:41 +0100

    Created CartItem model for Cart model

---
+++ muffinshop/MuffinShop/MuffinShopWPF/Model/CartItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuffinShopWPF.Model
+{
+    class CartItem
+    {
+        private Muffin product;
+
+        public Muffin Product
+        {
+            get { return product; }
+            set { product = value; }
+        }
+
+        private byte quantity;
+
+        public byte Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        private int price;
+
+        public int Price
+        {
+            get { return this.Product.muffin_price; }
+            set { price = value; }
+        }
+
+    }
+}
