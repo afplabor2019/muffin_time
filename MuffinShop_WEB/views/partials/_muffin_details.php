605dd95f6b61dda55bfa5b91b78a2ed3354cb460
vereczkibalint <vereczkibalint@gmail.com>
2019-12-17 08:27:47 +0100

    More validation added, implemented quantity selector

--- MuffinShop_WEB/views/partials/_muffin_details.php
+++ MuffinShop_WEB/views/partials/_muffin_details.php
@@ -3,8 +3,26 @@
 		<div class="container-fluid">
 		<?php
 			if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["add_to_cart"])){
-				array_push($_SESSION['cart'], $muffin["muffin_id"]);
-				redirect('cart', ['item_added' => 1]);
+				if(!logged_in()){
+					redirect('home');
+				}
+				$errors = [];
+				$qty = trim($_POST["qty"]);
+
+				if(!is_numeric($qty)){
+					$errors['qty'][] = "Csak számot írjon be!";
+				}else{
+					if($qty <= 0 || $qty > 10){
+						$errors['qty'][] = "A mennyiségnek 1 és 10 között kell lennie!";
+					}
+				}
+
+				if(count($errors) == 0){
+					for($i = 0; $i < $qty; $i++){
+						array_push($_SESSION['cart'], $muffin["muffin_id"]);
+					}
+					redirect('cart', ['item_added' => 1]);
+				}
 			}
 		?>
 			<form method="POST">
@@ -20,7 +38,7 @@
 						<h4 class="price">Ár <span><?=$muffin["muffin_price"]?> Ft</span></h4>
 
 						<div class="action mt-5">
-							<button class="btn btn-primary" type="submit" name="add_to_cart">Kosárba tesz</button>
+							<input type="number" class="form-control qtyCartInput<?php echo isset($errors['qty']) ? " has-error" : ''; ?>" min="1" max="10" name="qty" value="1"> <button class="btn btn-primary" type="submit" name="add_to_cart">Kosárba tesz</button>
 						</div>
 					</div>
 				</div>
